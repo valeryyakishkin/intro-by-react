fbb2d086134c4e39231d3a6082c413d8c2e08f9e
VelPaulov <velyakishkin@gmail.com>
2022-12-24 09:22:37 +0300

    added counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,39 @@ import { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  increaseCount() {
+    this.setState((state) => {
+      return {
+        ...state,
+        count: ++state.count
+      }
+    })
+  }
+
+  decreaseCount() {
+    this.setState((state) => {
+      return {
+        ...state,
+        count: --state.count
+      }
+    })
+  }
+  
   render() {
+    const { count } = this.state;
     return (
       <div>
-        <h1>Hello World</h1>
+        <button onClick={() => this.increaseCount()}>Plus</button>
+        <span>{count}</span>
+        <button onClick={() => this.decreaseCount()}>Minus</button>
       </div>
     )
   }
